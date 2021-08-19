9a271119570df1a35158f26b7e070e81d86e4c12
sanha33 <48049882+sanha33@users.noreply.github.com>
2021-08-19 15:08:00 +0900

    wegolego first ui

---
+++ wegolego_first_ui/lib/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "420a7cd4fcbb4727a519c1bacf4ece8a";
+//const Token = "006f98c3503352a4b86bf7ca15fd3a80ff7IACPUSQnNbL+mvaqxJ99FyktMxIN4FeI5UTovJ4BzeAQkQx+f9gAAAAAEAAM2hL3Zw/4YAEAAQBmD/hg";
