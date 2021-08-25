7ccef7b4ebdc804239a9644d6f0de06cac805357
sanha33 <48049882+sanha33@users.noreply.github.com>
2021-08-25 11:21:21 +0900

    (최종1)구글 로그인+페이지+기술 합친 프로젝트(auth 미포함)

---
+++ getx_bottom_navigation_bar/lib/pages/home/home_controller.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+import 'package:getx_bottom_navigation_bar/pages/first/first_controller.dart';
+
+class HomePageController extends GetxController{
+  final String title ="Home Page Title";
+
+  var tabIndex=0;
+  void changeTabIndex(int index){
+    tabIndex = index;
+    update();
+  }
+
+  FirstPageController firstController = Get.find<FirstPageController>();
+  late User user;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    user = Get.arguments;
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+
+  void logout() async {
+    await firstController.googleSign.disconnect();
+    await firstController.firebaseAuth.signOut();
+  }
+
+}
