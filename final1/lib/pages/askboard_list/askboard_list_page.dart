d272860cc0563f9a0a06aad04d3d5ceba21c9b12
sanha33 <48049882+sanha33@users.noreply.github.com>
2021-08-26 10:07:35 +0900

    auth + 기술 완벽히 합치기 완료

---
+++ final1/lib/pages/askboard_list/askboard_list_page.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:getx_bottom_navigation_bar/agorafunc/call_helper.dart';
+import 'package:getx_bottom_navigation_bar/agorafunc/call_taker.dart';
+
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListViewState createState() => _ListViewState();
+}
+
+class _ListViewState extends State<ListPage> {
+
+  ClientRole? _role = ClientRole.Audience;
+
+  String _displayText = 'Results go here';
+  final _database = FirebaseDatabase.instance.reference();
+  late StreamSubscription _uidStream;
+  final uid = 'uid';
+
+  // late DatabaseReference _uidRef;
+
+  @override
+  void initState() {
+    //  final FirebaseDatabase database = FirebaseDatabase(app: this.app);
+    //  _uidRef = database.reference().child('Uids');
+
+    super.initState();
+    //  _activateListeners();
+  }
+  //
+  // void _activateListeners() {
+  //   _uidStream = _database.child('Uids').onValue.listen((event) {
+  //     final data = new Map<String, dynamic>.from(event.snapshot.value);
+  //     final uid = UID.fromRTDB(data);
+  //     //final String? uid = event.snapshot.value;
+  //     setState(() {
+  //       _displayText = 'Channel name : $uid';
+  //     });
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    //  final ref = referenceDatabase.reference();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Channel List'),
+        ),
+        body: Center(
+          child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Column(
+                children: [
+
+                  StreamBuilder(
+                      stream: _database
+                          .child('Uids')
+                          .orderByKey()
+                      //.child('uid')
+                          .limitToLast(10)
+                          .onValue,
+                      builder: (context, snapshot) {
+                        final tilesList = <ListTile>[];
+                        if (snapshot.hasData) {
+                          final Channels = Map<String, dynamic>.from(
+                              (snapshot.data! as Event).snapshot.value);
+                          Channels.forEach((key, value) {
+                            final nextchannel =
+                            Map<String, dynamic>.from(value);
+                            final uidTile = ListTile(
+                                leading: Icon(Icons.favorite),
+                                title: Text(nextchannel['uid']),
+                                onTap: () => {
+                                  Get.to(() => CallPage_helper(
+                                    channelName: nextchannel['uid'],
+                                    //role: _role,
+                                  ))
+                                }
+                            );
+                            tilesList.add(uidTile);
+                          });
+                        }
+                        return Expanded(child: ListView(children: tilesList));
+                      })
+                ],
+              )),
+        ));
+  }
+}
